8fc87a88e7248280ecf890dc232fa3ed032e8a85
Dmitriy Rudakov <dmitriy.rudakov@quantori.com>
2024-09-09 09:58:45 +0200

    update packages & tsconfig

--- packages/fe/vite.config.ts
+++ packages/fe/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-});
+	plugins: [react(), viteTsconfigPaths(), svgr({ include: '**/*.svg?react' })],
+	server: { port: 3000 },
+})
