0e87b127fcbc10505899fe7d2778ee7d1433c143
Dmitriy Rudakov <dmitriy.rudakov@quantori.com>
2024-09-09 09:59:36 +0200

    create app config

---
+++ packages/fe/src/config/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+	isDev: import.meta.env.DEV,
+	isProd: import.meta.env.PROD,
+
+	apiUrl: `${import.meta.env.VITE_API_URL ?? 'http://localhost'}`,
+	apiPort: `${import.meta.env.VITE_API_PORT ?? '5000'}`,
+	apiRoute: `${import.meta.env.VITE_API_ROUTE ?? '/api/v1'}`,
+}
