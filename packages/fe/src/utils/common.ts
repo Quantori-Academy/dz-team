62c442b702033176f45bd598ebfcae7e26b881b2
Dmitriy Rudakov <dmitriy.rudakov@quantori.com>
2024-09-11 16:06:51 +0200

    add 'common' helpers

---
+++ packages/fe/src/utils/common.ts
@@ -0,0 +1,21 @@
+export const plural = (count?: number, noun?: string | null, fix = false, withoutCount = false) => {
+    if (count) {
+        const singular = fix && noun ? noun.replace(/s$/, "") : noun;
+        const countLabel = withoutCount ? "" : `${count} `;
+        return singular ? countLabel + (count === 1 ? singular : `${singular}s`) : String(count);
+    }
+    return undefined;
+};
+
+export function assert<T>(ok: T, message = "failed"): asserts ok {
+    if (!ok) {
+        const error = new Error(message);
+        if (typeof dev !== "undefined") dev.info("{!assertion}", message);
+        // eslint-disable-next-line no-console
+        else console.log("ASSERTION: " + message);
+
+        // TODO: maybe we'll use `Sentry` someday?
+        // trackError(message, error)
+        throw error;
+    }
+}
---
+++ packages/fe/src/utils/index.ts
@@ -0,0 +1,1 @@
+export { plural, assert } from "./common";
