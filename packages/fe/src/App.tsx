178d4b9d7a8a4653fa221cb6e8db3d1666015cc3
Dmitriy Rudakov <dmitriy.rudakov@quantori.com>
2024-09-11 15:51:12 +0200

    fixes based on new eslit rules

--- packages/fe/src/App.tsx
+++ packages/fe/src/App.tsx
@@ -1,20 +1,18 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+import { useEffect, useState } from "react";
+
+import reactLogo from "./assets/react.svg";
 import { isProd } from "./utils/isProd";
+
+import viteLogo from "/vite.svg";
 
 function App() {
     const [count, setCount] = useState(0);
     const [connectionState, setConnectionState] = useState("...");
 
     useEffect(() => {
-        fetch(
-            isProd
-                ? "http://vm4.quantori.academy:1337/"
-                : "http://localhost:1337/",
-        )
+        fetch(isProd ? "http://vm4.quantori.academy:1337/" : "http://localhost:1337/")
             .then((res) => {
                 if (res.ok) {
                     setConnectionState("ok!");
@@ -22,30 +20,24 @@ function App() {
             })
             .catch((err) => {
                 setConnectionState("offline, check console for details");
-                console.error(err);
+                dev.error("{!offline}", err);
             });
     }, []);
 
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
             <h1>Vite + React</h1>
             <h3>{isProd ? "Production build" : "Not production build"}</h3>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
                 <p>App runs ok!</p>
             </div>
             <p>Server connection is {connectionState}</p>
