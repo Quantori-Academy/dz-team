11330f7938deff6724c3255a50ab6f755af9937d
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-09-26 09:14:37 +0400

    fix file name and theme provider importing

--- packages/fe/src/App.tsx
+++ packages/fe/src/App.tsx
@@ -1,9 +1,8 @@
-import "./App.css";
 import "./logger/debug-load";
 
-import { ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material";
 
-import theme from "./Theme";
+import theme from "./theme";
 
 function App() {
     return <ThemeProvider theme={theme} />;
