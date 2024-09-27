601c0eedaaf6cab7539596375f51a010656cbc0c
vika pika <dbvicdb@gmail.com>
2024-09-27 23:55:19 +0200

    Add response data mapping

--- packages/fe/src/api/request.ts
+++ packages/fe/src/api/request.ts
@@ -1,11 +1,18 @@
 import ky, { Input, Options } from "ky";
 import * as rt from "runtypes";
 
-export async function request(url: Input, options?: Options & { contract?: rt.Runtype }) {
-    const response = await ky(url, options).json();
+export async function request<T, K>(
+    url: Input,
+    options?: Options & { contract?: rt.Runtype; mapper?: (val: T) => K }
+) {
+    const response = await ky<T>(url, options).json();
 
     if (options.contract) {
         options.contract.check(response);
+    }
+
+    if (options.mapper) {
+        return options.mapper(response);
     }
 
     return response;
