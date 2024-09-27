10915b03fceaefc318fc5dcd8ee1c13a87b24194
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-09-27 11:49:01 +0400

    style refactor

--- packages/fe/src/Theme.ts
+++ packages/fe/src/Theme.ts
@@ -3,19 +3,8 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
     palette: {},
     typography: {
-        fontFamily: "Arial, sans-serif",
         h1: {
             fontSize: "3.2rem",
-            fontWeight: "bold",
-        },
-    },
-    breakpoints: {
-        values: {
-            xs: 0,
-            sm: 600,
-            md: 960,
-            lg: 1280,
-            xl: 1920,
         },
     },
 });
