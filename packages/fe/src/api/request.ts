2931bfbb4f686deb2a33d2e54fdcbb35036b0c7b
vika pika <dbvicdb@gmail.com>
2024-09-27 20:56:12 +0200

    Add 'runtype' dependency and contract check

--- packages/fe/src/api/request.ts
+++ packages/fe/src/api/request.ts
@@ -1,5 +1,12 @@
 import ky, { Input, Options } from "ky";
+import * as rt from "runtypes";
 
-export function request(url: Input, options?: Options) {
-    return ky(url, options);
+export async function request(url: Input, options?: Options & { contract?: rt.Runtype }) {
+    const response = await ky(url, options).json();
+
+    if (options.contract) {
+        options.contract.check(response);
+    }
+
+    return response;
 }
