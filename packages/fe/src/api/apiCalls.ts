edc7b9a08613ad0cf536863d2a592269bc71538d
vika pika <dbvicdb@gmail.com>
2024-10-01 12:53:08 +0200

    Add mocks and use 'request' function to fetch molecules

--- packages/fe/src/api/apiCalls.ts
+++ packages/fe/src/api/apiCalls.ts
@@ -1,26 +1,17 @@
-import { config } from "config";
-
-const base = config.isProd ? "http://vm4.quantori.academy:1337" : "http://localhost:1337";
+import { base, request } from "./request";
 
 export const fetchServerConnection = async () => {
-    const response = await fetch(base);
-    if (response.ok) {
+    const result = await request(base + "/");
+
+    if (result) {
         return "ok!";
     }
 };
 
 export const fetchMolCount = async () => {
-    const response = await fetch(base + "/molecule/count");
-    if (response.ok) {
-        return ((await response.json()) as number).toString(); // TODO: types should be defined with schema
-    }
+    return request(base + "/molecule/count");
 };
 
-export const fetchMolPost = async () =>
-    await fetch(base + "/molecule", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ smiles: "CCO" }),
-    });
+export const fetchMolPost = async () => {
+    return request(base + "/molecule", { method: "post", json: { smiles: "CCO" } });
+};
--- packages/fe/src/api/request.ts
+++ packages/fe/src/api/request.ts
@@ -1,17 +1,39 @@
 import ky, { Input, Options } from "ky";
 import * as rt from "runtypes";
 
+import { config } from "config";
+import { wait } from "utils";
+
+const isDev = config.isDev;
+
+export const base = config.isProd ? "http://vm4.quantori.academy:1337" : "http://localhost:1337";
+
 export async function request<T, K>(
     url: Input,
     options?: Options & { contract?: rt.Runtype; mapper?: (val: T) => K }
 ) {
-    const response = await ky<T>(url, options).json();
+    const response = await ky<T>(url, {
+        ...options,
+        hooks: {
+            beforeRequest: [
+                async (request) => {
+                    if (isDev) {
+                        const mockData = (await import("./data.json")).default;
+                        const url = request.url.toString().replace(base, "");
+                        const mock = mockData[url];
+                        await wait(Math.random() * 1000);
+                        return new Response(JSON.stringify(mock), { status: 200 });
+                    }
+                },
+            ],
+        },
+    }).json();
 
-    if (options.contract) {
+    if (options?.contract) {
         options.contract.check(response);
     }
 
-    if (options.mapper) {
+    if (options?.mapper) {
         return options.mapper(response);
     }
 
