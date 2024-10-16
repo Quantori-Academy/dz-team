2857a759ba0e35cc8d4ad29dd1b922b5adb09b8b
VictoriaNechepurenko <dbvicdb@gmail.com>
2024-10-16 14:36:11 +0200

    Create DropdownMenuHeaderDesktop component

---
+++ packages/fe/src/components/header/desktop/DropdownMenuHeaderDesktop.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Logout, Settings } from "@mui/icons-material";
+import LanguageIcon from "@mui/icons-material/Language";
+import PersonIcon from "@mui/icons-material/Person";
+import { Box, Divider, ListItemIcon, Menu, MenuItem, Typography } from "@mui/material";
+import { theme } from "theme";
+
+export function DropdownMenuHeaderDesktop(props: {
+    handleClose: () => void;
+    open: boolean;
+    anchorEl: HTMLElement;
+}) {
+    return (
+        <Menu
+            anchorEl={props.anchorEl}
+            id="account-menu"
+            open={props.open}
+            onClose={props.handleClose}
+            onClick={props.handleClose}
+            slotProps={{
+                paper: {
+                    elevation: 1,
+                    sx: {
+                        overflow: "visible",
+                        filter: "drop-shadow(0px 2px 8px rgba(112,112,112,0.3))",
+                        mt: 4,
+                        "& .MuiAvatar-root": {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        "&::before": {
+                            content: '""',
+                            display: "block",
+                            position: "absolute",
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: "background.paper",
+                            transform: "translateY(-50%) rotate(45deg)",
+                            zIndex: 0,
+                        },
+                    },
+                },
+            }}
+            transformOrigin={{ horizontal: "center", vertical: "top" }}
+            anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+        >
+            <Box sx={{ mt: 1, mr: 2, mb: 1, ml: 2, width: "148px" }}>
+                <Typography
+                    sx={{
+                        color: theme.palette.text.primary,
+                    }}
+                >
+                    UserName
+                </Typography>
+                <Typography
+                    variant="body2"
+                    sx={{
+                        color: theme.palette.text.disabled,
+                    }}
+                >
+                    usermail@acme.com
+                </Typography>
+            </Box>
+            <Divider />
+            <MenuItem onClick={props.handleClose} sx={{ mt: 1 }}>
+                <ListItemIcon>
+                    <LanguageIcon fontSize="medium" />
+                </ListItemIcon>
+                Language
+            </MenuItem>
+            <MenuItem onClick={props.handleClose}>
+                <ListItemIcon>
+                    <PersonIcon fontSize="medium" />
+                </ListItemIcon>
+                Account
+            </MenuItem>
+            <MenuItem onClick={props.handleClose}>
+                <ListItemIcon>
+                    <Settings fontSize="medium" />
+                </ListItemIcon>
+                Settings
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={props.handleClose}>
+                <ListItemIcon>
+                    <Logout fontSize="medium" />
+                </ListItemIcon>
+                Logout
+            </MenuItem>
+        </Menu>
+    );
+}
--- packages/fe/src/components/header/desktop/index.tsx
+++ packages/fe/src/components/header/desktop/index.tsx
@@ -14,92 +14,109 @@ import {
 } from "@mui/material";
 import { Link } from "@tanstack/react-router";
 
+import { DropdownMenuHeaderDesktop } from "./DropdownMenuHeaderDesktop";
+
 import logo from "/lunaHeaderLogo.png";
 
 export function HeaderDesktop() {
     const theme = useTheme();
     const [isArrowUp, setIsArrowUp] = useState(false);
-    const handleArrowClick = () => {
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
         setIsArrowUp(!isArrowUp);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
     };
 
     return (
-        <AppBar elevation={1} sx={{ backgroundColor: theme.palette.background.default }}>
-            <Container maxWidth="lg">
-                <Toolbar disableGutters>
-                    {/* logo */}
-                    <Box display="flex" alignItems="center" sx={{ flexGrow: 1 }}>
-                        <Link to="/">
-                            <Box
-                                component="img"
-                                src={logo}
-                                alt="Luna LIMS Logo"
-                                sx={{ width: 62, height: 80, padding: "13px 0" }}
-                            />
-                        </Link>
-                    </Box>
+        <>
+            <AppBar elevation={1} sx={{ backgroundColor: theme.palette.background.default }}>
+                <Container maxWidth="lg">
+                    <Toolbar disableGutters>
+                        {/* logo */}
+                        <Box display="flex" alignItems="center" sx={{ flexGrow: 1 }}>
+                            <Link to="/">
+                                <Box
+                                    component="img"
+                                    src={logo}
+                                    alt="Luna LIMS Logo"
+                                    sx={{ width: 62, height: 80, padding: "13px 0" }}
+                                />
+                            </Link>
+                        </Box>
 
-                    {/* notification icon */}
-                    <IconButton
-                        sx={{
-                            mr: 5,
-                            color: theme.palette.text.disabled,
-                            "&:focus": { outline: "none" },
-                        }}
-                        disableRipple
-                    >
-                        <Notifications sx={{ width: 32, height: 32 }} />
-                    </IconButton>
+                        {/* notification icon */}
+                        <IconButton
+                            sx={{
+                                mr: 5,
+                                color: theme.palette.text.disabled,
+                                "&:focus": { outline: "none" },
+                            }}
+                            disableRipple
+                        >
+                            <Notifications sx={{ width: 32, height: 32 }} />
+                        </IconButton>
 
-                    {/* profile */}
-                    <Box
-                        display="flex"
-                        alignItems="center"
-                        onClick={handleArrowClick}
-                        sx={{ cursor: "pointer" }}
-                    >
-                        <Avatar sx={{ mr: 1 }} src="/path/to/avatar.jpg" alt="User Avatar" />
+                        {/* profile */}
+                        <Box
+                            onClick={handleClick}
+                            display="flex"
+                            alignItems="center"
+                            sx={{ cursor: "pointer" }}
+                        >
+                            <Avatar sx={{ mr: 1 }} src="/path/to/avatar.jpg" alt="User Avatar" />
 
-                        <Box display="flex" flexDirection="column">
-                            <Box display="flex" gap={"0 16px"}>
+                            <Box display="flex" flexDirection="column">
+                                <Box display="flex" gap={"0 16px"}>
+                                    <Typography
+                                        sx={{
+                                            color: theme.palette.text.primary,
+                                        }}
+                                    >
+                                        UserName
+                                    </Typography>
+                                    {isArrowUp ? (
+                                        <>
+                                            <KeyboardArrowUpIcon
+                                                sx={{
+                                                    color: theme.palette.text.disabled,
+                                                }}
+                                            />
+
+                                            <DropdownMenuHeaderDesktop
+                                                handleClose={handleClose}
+                                                open={open}
+                                                anchorEl={anchorEl}
+                                            />
+                                        </>
+                                    ) : (
+                                        <KeyboardArrowDownIcon
+                                            sx={{
+                                                color: theme.palette.text.disabled,
+                                            }}
+                                        />
+                                    )}
+                                </Box>
+
                                 <Typography
-                                    variant="body1"
+                                    variant="body2"
                                     sx={{
-                                        fontWeight: "regular",
-                                        color: theme.palette.text.primary,
+                                        color: theme.palette.text.disabled,
+                                        display: "flex",
+                                        justifyContent: "flex-start",
                                     }}
                                 >
-                                    UserName
+                                    Admin
                                 </Typography>
-                                {isArrowUp ? (
-                                    <KeyboardArrowUpIcon
-                                        sx={{
-                                            color: theme.palette.text.disabled,
-                                        }}
-                                    />
-                                ) : (
-                                    <KeyboardArrowDownIcon
-                                        sx={{
-                                            color: theme.palette.text.disabled,
-                                        }}
-                                    />
-                                )}
                             </Box>
-
-                            <Typography
-                                variant="body2"
-                                sx={{
-                                    color: theme.palette.text.disabled,
-                                    display: "flex",
-                                    justifyContent: "flex-start",
-                                }}
-                            >
-                                Admin
-                            </Typography>
                         </Box>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
     );
 }
