c4d4eb422ccc3a1802dfb04d8a7d56cb3236d9da
Art <absolutus11@gmail.com>
2024-10-16 11:31:42 +0400

    chore: remove autogenerated zod schema files

--- packages/shared/generated/zod/inputTypeSchemas/CategorySchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const CategorySchema = z.enum(["REAGENT", "SAMPLE"]);
-
-export type CategoryType = `${z.infer<typeof CategorySchema>}`;
-
-export default CategorySchema;
