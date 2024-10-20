dbe4830741e0dbdf381a13961b15d824eb84caf3
abe <absolutus11@gmail.com>
2024-10-20 22:14:31 +0400

    Feature/pagination sort filter search reagent (#130)
    * feat: modify reagent schema and prisma migration
    
    * feat: add db seed and reagent mock data
    
    * feat: add search, filtering, sorting, pagination query and validation
    
    * fix: add shared/generated/zod

--- packages/be/src/services/reagentService.ts
+++ packages/be/src/services/reagentService.ts
@@ -4,10 +4,87 @@ import {
     ReagentUpdateInputSchema,
 } from "shared/generated/zod/inputTypeSchemas";
 import { Reagent } from "shared/generated/zod/modelSchema";
+import { ReagentSearch } from "shared/zodSchemas";
 
 const prisma = new PrismaClient();
 
+type SearchResults = {
+    data: Reagent[];
+    meta: {
+        currentPage: number;
+        totalPages: number;
+        totalCount: number;
+        hasNextPage: boolean;
+        hasPreviousPage: boolean;
+    };
+};
+
 export class ReagentService {
+    /**
+     * Get all reagents that are not deleted.
+     * @returns {Promise<Reagent[]>} An array of all non-deleted reagents.
+     */
+    async getAllReagents(queryString: ReagentSearch): Promise<SearchResults> {
+        const {
+            query,
+            page,
+            limit,
+            sortBy,
+            sortOrder,
+            searchBy,
+            category,
+            storageLocation,
+            status,
+        } = queryString;
+
+        // conditionally construct search conditions
+        const searchConditions = query
+            ? searchBy?.map((field) => {
+                  return { [field]: { contains: query, mode: Prisma.QueryMode.insensitive } };
+              })
+            : null;
+
+        // combine filter and search conditions for a query
+        // <category> AND <status> AND <storageLocation> AND <query>[<name> OR <structure> OR ...]
+        const where: Prisma.ReagentWhereInput = {
+            // FILTER CONDITIONS
+            AND: [
+                category ? { category } : {},
+                status ? { status } : {},
+                storageLocation ? { storageLocation } : {},
+                // SEARCH CONDITIONS
+                // include OR condition only if there are search conditions
+                searchConditions && searchConditions.length > 0 ? { OR: searchConditions } : {},
+            ].filter(Boolean), // remove all {} conditions from AND condition
+            deletedAt: null, // exclude soft-deleted records
+        };
+
+        const [reagents, totalCount] = await Promise.all([
+            prisma.reagent.findMany({
+                where,
+                skip: (page - 1) * limit,
+                take: limit,
+                orderBy: { [sortBy]: sortOrder },
+            }),
+            prisma.reagent.count({ where }),
+        ]);
+
+        const totalPages = Math.ceil(totalCount / limit);
+
+        const result = {
+            data: reagents,
+            meta: {
+                currentPage: page,
+                totalPages,
+                totalCount,
+                hasNextPage: page < totalPages,
+                hasPreviousPage: page > 1,
+            },
+        };
+
+        return result;
+    }
+
     /**
      * Get a specific reagent by its ID.
      * @param {string} id - The UUID of the reagent to retrieve.
@@ -18,16 +95,8 @@ export class ReagentService {
     }
 
     /**
-     * Get all reagents that are not deleted.
-     * @returns {Promise<Reagent[]>} An array of all non-deleted reagents.
-     */
-    async getAllReagents(): Promise<Reagent[]> {
-        return await prisma.reagent.findMany({ where: { deletedAt: null } });
-    }
-
-    /**
      * Create a new reagent.
-     * @param {unknown} newReagentData - The data to create a new reagent.
+     * @param {Prisma.ReagentCreateInput} newReagentData - The data to create a new reagent.
      * @returns {Promise<Reagent>} The newly created reagent.
      */
     async createReagent(newReagentData: Prisma.ReagentCreateInput): Promise<Reagent> {
@@ -39,7 +108,7 @@ export class ReagentService {
     /**
      * Update an existing reagent by its ID.
      * @param {string} id - The UUID of the reagent to update.
-     * @param {unknown} updateReagentData - The data to update the reagent.
+     * @param {Prisma.ReagentUpdateInput} updateReagentData - The data to update the reagent.
      * @returns {Promise<Reagent>} The updated reagent.
      */
     async updateReagent(
