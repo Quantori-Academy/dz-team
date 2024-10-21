81164505bd821fe56388ee6cb3955b4898cdf66d
VictoriaNechepurenko <dbvicdb@gmail.com>
2024-10-21 15:55:28 +0200

    Implement login state in loginForm

--- packages/fe/src/components/pages/login/LoginForm.tsx
+++ packages/fe/src/components/pages/login/LoginForm.tsx
@@ -1,4 +1,6 @@
+import { ChangeEvent, useEffect, useState } from "react";
 import {
+    Alert,
     Box,
     Button,
     Checkbox,
@@ -8,11 +10,28 @@ import {
     TextField,
     Typography,
 } from "@mui/material";
+import { useNavigate } from "@tanstack/react-router";
+import { useUnit } from "effector-react";
 
+import { $loginState, loginFx } from "stores/login";
 import { useIsDesktop } from "utils/useIsDesktop";
 
 export function LoginForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [usernameError, setUsernameError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const navigate = useNavigate();
+
+    const loginState = useUnit($loginState);
     const isDesktop = useIsDesktop();
+
+    useEffect(() => {
+        if (loginState.success) {
+            navigate({ to: "/" });
+        }
+    }, [loginState.success, navigate]);
+
     return (
         <Box
             sx={{
@@ -48,15 +67,25 @@ export function LoginForm() {
             >
                 <Typography sx={{ fontSize: "24px" }}>Log In</Typography>
                 <TextField
-                    label="Username"
+                    error={!!usernameError || !!loginState.message}
+                    label={usernameError || "Username"}
                     variant="outlined"
                     sx={{ height: "56px", width: "100%", mt: 3, mb: 2 }}
+                    value={username}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                        setUsername(event.target.value);
+                    }}
                 />
                 <TextField
-                    label="Password"
+                    error={!!passwordError || !!loginState.message}
+                    label={passwordError || "Password"}
                     variant="outlined"
                     type="password"
                     sx={{ height: "56px", width: "100%", mb: 2 }}
+                    value={password}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                        setPassword(event.target.value);
+                    }}
                 />
                 <FormControlLabel
                     control={<Checkbox sx={{ ml: 1 }} />}
@@ -64,6 +93,19 @@ export function LoginForm() {
                     sx={{ mb: 2 }}
                 />
                 <Button
+                    onClick={() => {
+                        if (!username) {
+                            setUsernameError("Username is required");
+                        }
+                        if (!password) {
+                            setPasswordError("Password is required");
+                        }
+                        if (username && password) {
+                            loginFx({ username, password });
+                            setPasswordError("");
+                            setUsernameError("");
+                        }
+                    }}
                     variant="contained"
                     color="primary"
                     sx={{
@@ -74,6 +116,11 @@ export function LoginForm() {
                 >
                     Log In
                 </Button>
+                {loginState.message ? (
+                    <Alert severity="error" sx={{ mb: 2 }}>
+                        Failed to log in: incorrect username or password
+                    </Alert>
+                ) : null}
                 <Link variant="body2" color="secondary" sx={{ cursor: "pointer" }}>
                     Forgot password?
                 </Link>
---
+++ packages/fe/src/stores/login.ts
@@ -0,0 +1,17 @@
+import { genericDomain } from "logger";
+
+export const loginFx = genericDomain.createEffect(
+    ({ username, password }: { username: string; password: string }) => {
+        // the request function doesn't support logic, so we use a mock here
+        if (username === "admin" && password === "password") {
+            return { success: true };
+        } else {
+            throw new Error("Invalid credentials");
+        }
+    },
+);
+
+export const $loginState = genericDomain
+    .createStore<{ success: boolean; message: string | null }>({ success: false, message: null })
+    .on(loginFx.done, () => ({ success: true, message: null }))
+    .on(loginFx.fail, (_, { error }) => ({ success: false, message: error.message }));
