cb43c33d1229427f15f2d1c5acf3b1f461e0dbd2
Akimishvili <liliana.aqimishvili@gmail.com>
2024-10-25 13:43:02 +0400

    add basic functional of user management

--- packages/be/src/routes/apiRoutes.ts
+++ packages/be/src/routes/apiRoutes.ts
@@ -1,10 +1,12 @@
 import { reagentRoutes } from "./reagentRoutes";
 import { sampleRoutes } from "./sampleRoutes";
 import { authRoutes } from "./authRoutes";
+import { userRoutes } from "./userRoutes";
 import { FastifyZodInstance } from "../types";
 
 export const apiRoutes = async (app: FastifyZodInstance) => {
     app.register(reagentRoutes, { prefix: "/reagents" });
     app.register(sampleRoutes, { prefix: "/samples" });
     app.register(authRoutes, { prefix: "/auth" });
+    app.register(userRoutes, { prefix: "/users" });
 };
