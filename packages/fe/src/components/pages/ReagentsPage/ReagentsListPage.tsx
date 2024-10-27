f9f0f68e3454dff529c1249f0032790a0524eb5b
adletb7 <bah_92adlet@mail.ru>
2024-10-27 17:13:12 +0500

    edit style

--- packages/fe/src/components/pages/ReagentsPage/ReagentsListPage.tsx
+++ packages/fe/src/components/pages/ReagentsPage/ReagentsListPage.tsx
@@ -62,6 +62,13 @@ export const ReagentsListPage = () => {
                         bgcolor: "primary.main",
                         borderRadius: "4px 4px 0 0",
                         mb: -1,
+                        marginRight: "auto",
+                        background: "linear-gradient(0deg, #BFBFBF, #BFBFBF), #BFBFBF",
+                        color: "#000000",
+                        fontFamily: "Inter, sans-serif",
+                        fontWeight: 600,
+                        fontSize: "14px",
+                        lineHeight: "17px",
                     }}
                 >
                     Add Reagent
