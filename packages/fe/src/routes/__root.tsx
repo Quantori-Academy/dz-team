45e8f78d1412341b8b9c0a11b592719ebc8f5dda
Zhukov Dmitry <dmitriy.zhukov@quantori.com>
2024-10-30 13:52:36 +0400

    feat: add router devtools to fe dev mode

--- packages/fe/src/routes/__root.tsx
+++ packages/fe/src/routes/__root.tsx
@@ -1,12 +1,29 @@
+import { lazy, Suspense } from "react";
 import { createRootRouteWithContext } from "@tanstack/react-router";
 import { App } from "App";
 
+import { config } from "config";
 import { Auth } from "stores/auth";
+
+const TanStackRouterDevtools = config.isProd
+    ? () => null
+    : lazy(() =>
+          import("@tanstack/router-devtools").then((res) => ({
+              default: res.TanStackRouterDevtools,
+          })),
+      );
 
 type MyRouterContext = {
     auth: Auth | null;
 };
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-    component: () => <App />,
+    component: () => (
+        <>
+            <App />
+            <Suspense fallback={null}>
+                <TanStackRouterDevtools />
+            </Suspense>
+        </>
+    ),
 });
