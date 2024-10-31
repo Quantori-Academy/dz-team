bfb6be90db9db5b3e6645759fb54d0623b574d43
abe <absolutus11@gmail.com>
2024-10-31 14:56:35 +0400

    fix: add initial admin account (#189)

--- packages/be/prisma/seed.js
+++ packages/be/prisma/seed.js
@@ -1,18 +1,47 @@
 const { PrismaClient } = require("@prisma/client");
 const reagents = require("./seedData/reagents.json");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const prisma = new PrismaClient();
 
+const secret = process.env.JWT_SECRET || "b82359e8-f0a5-41d4-8677-2180f836d8cd";
+
+async function addAdmin() {
+    const adminCount = await prisma.user.count({ where: { role: "admin" } });
+    if (!adminCount) {
+        const p4$$w0rd = await bcrypt.hash("admin123", 10);
+        const admin = await prisma.user.create({
+            data: {
+                username: "admin",
+                password: p4$$w0rd,
+                role: "admin",
+                firstName: "admin",
+                lastName: "admin",
+                email: "admin@vm4.net",
+            },
+        });
+        const token = jwt.sign({ userId: admin.id, role: admin.role }, secret, { expiresIn: "1h" });
+        console.log(`🚀 Admin token: ${token}`);
+    } else {
+        console.log("🗿 No new admin was created.");
+    }
+}
+
 async function addReagents() {
-    const result = await prisma.reagent.createMany({
-        data: reagents,
-        skipDuplicates: true,
-    });
-    console.log(`Seeded database with ${result.count} reagent entries.`);
+    const reagentCount = await prisma.reagent.count();
+    if (!reagentCount) {
+        const result = await prisma.reagent.createMany({
+            data: reagents,
+            skipDuplicates: true,
+        });
+        console.log(`💉 Seeded database with ${result.count} reagent entries.`);
+    }
+    console.log("🥀 No new reagents were added.");
 }
 
 async function main() {
-    // comment out to avoid duplicate data
+    await addAdmin();
     await addReagents();
 }
 
