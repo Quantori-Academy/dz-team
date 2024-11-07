e8d61874ed6947d953271c9a34ee35fd516296c2
Art <absolutus11@gmail.com>
2024-11-08 02:21:57 +0400

    feat: combined list service, route, controller and zod schema

--- packages/be/src/routes/apiRoutes.ts
+++ packages/be/src/routes/apiRoutes.ts
@@ -1,3 +1,4 @@
+import { combinedListRoutes } from "./combinedListRoutes";
 import { reagentRoutes } from "./reagentRoutes";
 import { sampleRoutes } from "./sampleRoutes";
 import { authRoutes } from "./authRoutes";
@@ -6,6 +7,7 @@ import { FastifyZodInstance } from "../types";
 import { storageLocationRoutes } from "./storageLocationRoutes";
 
 export const apiRoutes = async (app: FastifyZodInstance) => {
+    app.register(combinedListRoutes, { prefix: "/list" });
     app.register(reagentRoutes, { prefix: "/reagents" });
     app.register(sampleRoutes, { prefix: "/samples" });
     app.register(authRoutes, { prefix: "/auth" });
---
+++ packages/be/src/routes/combinedListRoutes.ts
@@ -0,0 +1,46 @@
+import { CombinedListController } from "../controllers/combinedListController";
+import { FastifyZodInstance } from "../types";
+import { CombinedListSearch } from "../../../shared/zodSchemas";
+
+const combinedListController = new CombinedListController();
+
+/**
+ * Registers the combined list route with the provided Fastify instance.
+ *
+ * @param {FastifyZodInstance} app - The Fastify instance to register the routes with.
+ * @returns {Promise<void>} A promise that resolves when the routes have been registered.
+ */
+export const combinedListRoutes = async (app: FastifyZodInstance): Promise<void> => {
+    /**
+     * GET / endpoint to retrieve a list of reagents and samples.
+     *
+     * @name GetCombinedList
+     * @function
+     * @memberof module:routes
+     * @param {import("shared/zodSchemas").CombinedListSearch} request.query - The search parameters for filtering reagents and samples.
+     * @param {Object} reply - The reply object.
+     * @returns {Promise<Object>} The list of reagents and samples and metadata.
+     * @throws {Error} Throws an error if there is an issue retrieving reagents and samples.
+     *
+     * @typedef {Object} CombinedListSearch
+     * @property {number} [page] - The page number for pagination.
+     * @property {number} [limit] - The number of results per page.
+     * @property {string} [sortBy] - The field to sort the results by.
+     * @property {'asc' | 'desc'} [sortOrder] - The order to sort the results.
+     * @property {string} [category] - The category of item to filter by.
+     * @property {string} [status] - The status of reagents to filter by.
+     * @property {string} [storageLocation] - The storage location to filter by.
+     * @property {string} [query] - A search query to filter results.
+     *
+     * @example
+     * // Example query
+     * GET /?page=1&limit=20&sortBy=name&sortBy=structure&sortOrder=asc
+     */
+    app.get<{ Querystring: CombinedListSearch }>(
+        "/",
+        { schema: { tags: ["CombinedList"] } },
+        async (request, reply) => {
+            return await combinedListController.getCombinedList(request, reply);
+        },
+    );
+};
