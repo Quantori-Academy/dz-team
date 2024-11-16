f3b2d45468b2b98cb8b0af674f1765774e47d291
nickgabriadze <nickgabriadze@gmail.com>
2024-11-17 00:12:35 +0400

    feat: add zod openapi type provider to instance

--- packages/be/src/types.ts
+++ packages/be/src/types.ts
@@ -8,7 +8,7 @@ import {
     FastifyReply,
 } from "fastify";
 
-import { ZodTypeProvider } from "fastify-type-provider-zod";
+import type { FastifyZodOpenApiTypeProvider } from "fastify-zod-openapi";
 
 // Define the JWT payload type with roles
 export interface JwtPayload {
@@ -37,7 +37,7 @@ export type FastifyZodInstance = FastifyInstance<
     RawRequestDefaultExpression<RawServerDefault>,
     RawReplyDefaultExpression<RawServerDefault>,
     FastifyBaseLogger,
-    ZodTypeProvider
+    FastifyZodOpenApiTypeProvider
 > & {
     verifyJWT?: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
     verifyRole?: (
