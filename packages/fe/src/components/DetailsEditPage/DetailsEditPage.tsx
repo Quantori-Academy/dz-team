0e53e7603878a6cc96c9f7fe071c5ce734717893
adletb7 <bah_92adlet@mail.ru>
2024-11-17 21:26:30 +0500

    props shorter version

--- packages/fe/src/components/DetailsEditPage/DetailsEditPage.tsx
+++ packages/fe/src/components/DetailsEditPage/DetailsEditPage.tsx
@@ -22,20 +22,11 @@ type DetailsEditPageProps<T extends AnyRoute, TData> = {
     editableFields?: string[];
 };
 
-export const DetailsEditPage = <T extends AnyRoute, TData>({
-    baseUrl,
-    url,
-    fields,
-    onAction,
-    editableFields,
-}: DetailsEditPageProps<T, TData>) => {
+export const DetailsEditPage = <T extends AnyRoute, TData>(
+    props: DetailsEditPageProps<T, TData>,
+) => {
     const { ref } = useContext(TableContext);
-    return (
-        <DetailsEditPageInner
-            {...{ baseUrl, url, fields, onAction, editableFields }}
-            tableRef={ref}
-        />
-    );
+    return <DetailsEditPageInner {...props} tableRef={ref} />;
 };
 export function DetailsEditPageInner<T extends AnyRoute, TData>({
     baseUrl,
