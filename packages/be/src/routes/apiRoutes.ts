873c85114520790f2884bb33e593c483c9bc91cf
Gani Uapov <gankon17@gmail.com>
2024-11-21 14:12:12 +0500

    fix: update routes file wit adding /requests route

--- packages/be/src/routes/apiRoutes.ts
+++ packages/be/src/routes/apiRoutes.ts
@@ -5,7 +5,7 @@ import { userRoutes } from "./userRoutes";
 import { FastifyZodInstance } from "../types";
 import { storageLocationRoutes } from "./storageLocationRoutes";
 import { orderRoutes } from "./orderRoutes";
-
+import { requestRoutes } from "./requestRoutes";
 export const apiRoutes = async (app: FastifyZodInstance) => {
     app.register(reagentRoutes, { prefix: "/reagents" });
     app.register(sampleRoutes, { prefix: "/samples" });
@@ -13,4 +13,5 @@ export const apiRoutes = async (app: FastifyZodInstance) => {
     app.register(userRoutes, { prefix: "/users" });
     app.register(storageLocationRoutes, { prefix: "/storage-locations" });
     app.register(orderRoutes, { prefix: "/orders" });
+    app.register(requestRoutes, { prefix: "/requests" });
 };
