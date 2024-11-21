cb98520b5375e92e41a1962389eb27aab833c699
adletb7 <bah_92adlet@mail.ru>
2024-11-21 19:05:11 +0500

    detail/edit page with grid approach to show reagents

--- packages/fe/src/api/orderDetails/getOrderDetails.ts
+++ packages/fe/src/api/orderDetails/getOrderDetails.ts
@@ -1,6 +1,6 @@
 import { base, request } from "api/request";
+import { OrderSchema } from "shared/generated/zod/modelSchema";
 
-import { OrderDetailsContract } from "./contract";
 
 export const getOrdersDetailsApi = async ({ id }: { id: string }) =>
-    await request(`${base}/api/v1/orders/${id}`, OrderDetailsContract);
+    await request(`${base}/api/v1/orders/${id}`, OrderSchema);
