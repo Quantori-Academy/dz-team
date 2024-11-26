5100a282c3d1d653375f02835a46a132c54e0005
Akimishvili <125081600+Akimishvili@users.noreply.github.com>
2024-11-27 00:22:45 +0400

    add combined list functional for BE

---
+++ packages/be/prisma/migrations/20241126200206_add_combined_list/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "CombinedList" (
+    "id" UUID NOT NULL,
+    "category" TEXT,
+    "name" TEXT,
+    "structure" TEXT,
+    "description" TEXT,
+    "quantity" DOUBLE PRECISION,
+    "unit" TEXT,
+    "quantityInit" DOUBLE PRECISION,
+    "expirationDate" TIMESTAMP(3),
+    "producer" TEXT,
+    "catalogId" TEXT,
+    "catalogLink" TEXT,
+    "pricePerUnit" DOUBLE PRECISION,
+    "currency" TEXT,
+    "type" "ReagentType",
+    "status" "ReagentStatus",
+    "cas" TEXT,
+    "storageLocation" TEXT,
+    "deletedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3),
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "CombinedList_pkey" PRIMARY KEY ("id")
+);
--- packages/be/src/routes/apiRoutes.ts
+++ packages/be/src/routes/apiRoutes.ts
@@ -5,8 +5,10 @@ import { userRoutes } from "./userRoutes";
 import { FastifyZodInstance } from "../types";
 import { storageLocationRoutes } from "./storageLocationRoutes";
 import { orderRoutes } from "./orderRoutes";
+import { combinedListRoutes } from "./combinedListRoutes";
 
 export const apiRoutes = async (app: FastifyZodInstance) => {
+    app.register(combinedListRoutes, { prefix: "/list" });
     app.register(reagentRoutes, { prefix: "/reagents" });
     app.register(sampleRoutes, { prefix: "/samples" });
     app.register(authRoutes, { prefix: "/auth" });
