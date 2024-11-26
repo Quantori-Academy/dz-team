953516d93da01c1c8dd9791214e6e44e79661132
VictoriaNechepurenko <dbvicdb@gmail.com>
2024-11-26 15:04:03 +0100

    Fix comments

--- packages/fe/src/components/pages/reagentRequestPage/ReagentRequestPage.tsx
+++ packages/fe/src/components/pages/reagentRequestPage/ReagentRequestPage.tsx
@@ -1,8 +1,10 @@
+import { useRef } from "react";
 import { GridColDef } from "@mui/x-data-grid";
 import { Outlet, useNavigate } from "@tanstack/react-router";
 
 import { base } from "api/request";
-import { CommonTable } from "components/commonTable/CommonTable";
+import { CommonTable, CommonTableRef } from "components/commonTable/CommonTable";
+import { TableContext } from "components/commonTable/TableContext";
 
 import { ReagentRequest, ReagentRequestSchema } from "../../../../../shared/generated/zod";
 
@@ -13,6 +15,7 @@ const reagentRequestColumns: GridColDef[] = [
     { field: "quantity", headerName: "Desired Quantity", width: 150 },
     { field: "unit", headerName: "Unit", width: 150 },
     { field: "status", headerName: "Status", width: 150 },
+    { field: "userId", headerName: "User ID", width: 150 },
     { field: "userComments", headerName: "User Comments", width: 200 },
     { field: "procurementComments", headerName: "Procurement Comments", width: 200 },
     { field: "createdAt", headerName: "Date Created", width: 150 },
@@ -21,15 +24,17 @@ const reagentRequestColumns: GridColDef[] = [
 
 export function ReagentRequestPage() {
     const navigate = useNavigate();
+    const tableRef = useRef<CommonTableRef | null>(null);
 
     const handleRowClick = (row: ReagentRequest) => {
         navigate({ to: `/reagentRequests/${row.id}`, replace: false });
     };
 
     return (
-        <>
+        <TableContext.Provider value={{ ref: tableRef }}>
             <CommonTable<ReagentRequest>
                 columns={reagentRequestColumns}
+                ref={tableRef}
                 url={`${base}/api/v1/reagent-request`}
                 schema={ReagentRequestSchema}
                 onRowClick={handleRowClick}
@@ -47,6 +52,6 @@ export function ReagentRequestPage() {
             />
 
             <Outlet />
-        </>
+        </TableContext.Provider>
     );
 }
