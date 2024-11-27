d7cd79f4571a94b171bf7e06c1bdcde22cd8582e
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-11-28 00:31:43 +0400

    remove get storage request

--- packages/fe/src/api/storage/getStorage.ts
+++
@@ -1,8 +0,0 @@
-import { base, request } from "../request";
-import { contractStorageType } from "./contract";
-
-export const getStorage = async () => {
-    const storage = await request(`${base}/api/v1/storage-locations`, contractStorageType);
-    const storageData = storage?.data;
-    return storageData;
-};
