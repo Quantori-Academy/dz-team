7d0439dd23154d7e27d1558a94147d0e05b13bab
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-12-06 02:10:22 +0400

    Remove changes to samples.tsx from the branch

--- packages/fe/src/routes/_app/_researcherLayout/samples.tsx
+++ packages/fe/src/routes/_app/_researcherLayout/samples.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/_researcherLayout/samples")({
-    component: () => <div>hello samples</div>,
+    component: () => <div>Hello, samples!</div>,
 });
