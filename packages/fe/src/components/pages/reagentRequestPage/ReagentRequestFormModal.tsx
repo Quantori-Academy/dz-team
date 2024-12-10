095bb34b772bba965b09d1885cf1596b5420e7af
VictoriaNechepurenko <dbvicdb@gmail.com>
2024-12-10 13:16:50 +0100

    Add unit selector

--- packages/fe/src/components/pages/reagentRequestPage/ReagentRequestFormModal.tsx
+++ packages/fe/src/components/pages/reagentRequestPage/ReagentRequestFormModal.tsx
@@ -1,4 +1,13 @@
-import { BaseTextFieldProps, Grid2 as Grid, TextField } from "@mui/material";
+import {
+    BaseTextFieldProps,
+    FormControl,
+    Grid2 as Grid,
+    InputLabel,
+    MenuItem,
+    Select,
+    SelectChangeEvent,
+    TextField,
+} from "@mui/material";
 import { useUnit } from "effector-react";
 
 import {
@@ -18,17 +27,21 @@ const fields: BaseTextFieldProps[] = [
         name: "quantity",
         type: "number",
     },
-    { label: "Unit", name: "unit", helperText: "Available options: ml, l, mg, g, oz, lb." },
+    { label: "Unit", name: "unit" },
     { label: "User Comments", name: "commentsUser", type: "array" },
     { label: "Procurement Comments", name: "commentsProcurement", type: "array", disabled: true },
     { label: "Status", name: "status", disabled: true },
 ];
 
+const unitOptions = ["ml", "l", "mg", "g", "oz", "lb"];
+
 export const ReagentRequestFormModal = () => {
     const [formData, formDataErrors, touch] = useUnit([$formData, $formDataErrors, touchField]);
     const shouldShowErrors = useUnit($shouldShowErrors);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent<string>,
+    ) => {
         const { name, value } = e.target;
         setFormData({
             ...formData,
@@ -38,31 +51,51 @@ export const ReagentRequestFormModal = () => {
 
     return (
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            {fields.map(({ name, label, type, helperText, required, disabled, ...rest }) => {
+            {fields.map(({ name, label, type, required, disabled, ...rest }) => {
                 const errorText = formDataErrors[name as keyof typeof formDataErrors];
                 const showError = shouldShowErrors[name as keyof typeof formDataErrors];
+
                 return (
                     <Grid size={6} key={name}>
-                        <TextField
-                            label={label}
-                            name={name}
-                            value={formData[name as keyof typeof formData] || ""}
-                            onChange={handleChange}
-                            fullWidth
-                            margin="normal"
-                            type={type || "text"}
-                            onBlur={() => {
-                                if (name) {
-                                    touch(name);
-                                }
-                            }}
-                            helperText={showError ? errorText || helperText : ""}
-                            error={showError ? Boolean(errorText) : false}
-                            required={required}
-                            disabled={disabled}
-                            sx={{ width: 1 }}
-                            {...rest}
-                        />
+                        {name === "unit" ? (
+                            <FormControl fullWidth sx={{ width: 1, mt: 2, mb: 1 }}>
+                                <InputLabel id="unit-label">{label}</InputLabel>
+                                <Select
+                                    labelId="unit-label"
+                                    label={label}
+                                    name={name}
+                                    value={formData[name]}
+                                    onChange={handleChange}
+                                >
+                                    {unitOptions.map((option) => (
+                                        <MenuItem key={option} value={option}>
+                                            {option}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        ) : (
+                            <TextField
+                                label={label}
+                                name={name}
+                                value={formData[name as keyof typeof formData] || ""}
+                                onChange={handleChange}
+                                fullWidth
+                                margin="normal"
+                                type={type || "text"}
+                                onBlur={() => {
+                                    if (name) {
+                                        touch(name);
+                                    }
+                                }}
+                                helperText={showError ? errorText : ""}
+                                error={showError ? Boolean(errorText) : false}
+                                required={required}
+                                disabled={disabled}
+                                sx={{ width: 1 }}
+                                {...rest}
+                            />
+                        )}
                     </Grid>
                 );
             })}
