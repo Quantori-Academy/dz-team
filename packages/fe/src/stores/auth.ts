3bf6175a7ca7d91f0e52b39ba5c54ea8b44bca0d
VictoriaNechepurenko <dbvicdb@gmail.com>
2024-12-11 09:23:52 +0100

    Remove unused userId store

--- packages/fe/src/stores/auth.ts
+++ packages/fe/src/stores/auth.ts
@@ -11,14 +11,6 @@ export type AuthStoreValue = Auth | null | false;
 
 export const $auth = genericDomain.createStore<AuthStoreValue>(false);
 
-export const $userId = $auth.map((auth) => {
-    if (auth && auth.token) {
-        const decodedToken = jwtDecode<{ userId: string }>(auth.token);
-        return decodedToken.userId || null;
-    }
-    return null;
-});
-
 export const getUserFx = genericDomain.createEffect(
     ({ token, userId }: { token: string; userId: string }) => {
         try {
@@ -47,6 +39,7 @@ export const sessionLoadFx = genericDomain.createEffect(() => {
         const decodedToken: { userId: string } = jwtDecode(parsed.token);
         return { ...parsed, userId: decodedToken.userId };
     }
+
     return parsed;
 });
 
