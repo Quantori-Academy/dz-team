a5699f526a9f95150954381982d24c635ba00e73
Zhukov Dmitry <dmitriy.zhukov@quantori.com>
2024-12-11 18:05:38 +0400

    hack: remove devtools from local

--- packages/fe/src/routes/__root.tsx
+++ packages/fe/src/routes/__root.tsx
@@ -1,29 +1,12 @@
-import { lazy, Suspense } from "react";
 import { createRootRouteWithContext } from "@tanstack/react-router";
 import { App } from "App";
 
-import { config } from "config";
 import { AuthStoreValue } from "stores/auth";
-
-const TanStackRouterDevtools = config.isProd
-    ? () => null
-    : lazy(() =>
-          import("@tanstack/router-devtools").then((res) => ({
-              default: res.TanStackRouterDevtools,
-          })),
-      );
 
 type MyRouterContext = {
     auth: AuthStoreValue;
 };
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-    component: () => (
-        <>
-            <App />
-            <Suspense fallback={null}>
-                <TanStackRouterDevtools />
-            </Suspense>
-        </>
-    ),
+    component: () => <App />,
 });
